1ac0d125e21dd4b75ef7120da5c62ae7147bdb40
[leonard.achanya] <[lennardachanya.la@gmail.com]>
2019-03-08 15:31:30 +0100

    added the redirect on login succesful

--- src/components/Menu/Login/Login.js
+++ src/components/Menu/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import AuthContext from "../../Authprovider/Authcontext";
 
 import './Login.css';
@@ -11,12 +12,17 @@ const Login = () => {
             <AuthContext.Consumer>
                 {
                     change => (
+                        change.state.isAuthenticated ?
+                        (<Redirect to ="/profile"/>)
+                        :
+                        (
                         <div className="box">
                         {change.state.error}
                             <input type="text" name="username" placeholder="Enter your Username" onChange={change.userChange}></input>
                             <input type="text" name="password" placeholder="Enter your Password" onChange={change.passwordChange}></input>
                             <button type="submit" disabled={!change.state.username || !change.state.password} onClick={change.login}>Login</button>
                         </div>
+                        )
                     )
                 }
             </AuthContext.Consumer>
