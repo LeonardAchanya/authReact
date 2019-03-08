d353dd6518991dbdef64b91a8aaf242988e14fce
[leonard.achanya] <[lennardachanya.la@gmail.com]>
2019-03-08 10:29:40 +0100

    fixed the redirect on authentication

--- src/components/Menu/Profile/Profile.js
+++ src/components/Menu/Profile/Profile.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import AuthContext from "../../Authprovider/Authcontext";
 
 
-const Profile = ()=>{
-    return(
-        <>
-        <h3>Profile</h3>
-        </>
+const Profile = () => {
+    return (
+
+        <AuthContext.Consumer>
+            {context =>
+                !context.state.isAuthenticated ? (
+                    <>
+                        <Redirect to="/login" />
+                    </>
+                ) : (
+
+                        <>
+                            <h3>Profile</h3>
+                        </>
+                    )
+            }
+        </AuthContext.Consumer>
+
     )
 }
 
--- src/components/Menu/Settings/Settings.js
+++ src/components/Menu/Settings/Settings.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import AuthContext from "../../Authprovider/Authcontext";
 
 
-const Settings = ()=>{
-    return(
-        <>
-        <h3>Settings</h3>
-        </>
+const Settings = () => {
+    return (
+
+        <AuthContext.Consumer>
+            {context =>
+                !context.state.isAuthenticated ? (
+                    <>
+                        <Redirect to="/login" />
+                    </>
+                ) : (
+
+                        <>
+                            <h3>Settings</h3>
+                        </>
+                    )
+            }
+        </AuthContext.Consumer>
+
     )
 }
 
